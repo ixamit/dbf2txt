7f0a2e566b69611d8e44e10bf02d4c37ce4e942f
ixamit <ixamit@gmail.com>
2012-10-12 11:10:08 +0200

    add header

---
+++ lib/dump_ASCII.h
@@ -0,0 +1,27 @@
+/**
+ *  Copyright (C) 2012, Max Cavallo <ixamit_at_gmail_dot_com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+*/
+#ifndef _DUMP_ASCII_H
+#define _DUMP_ASCII_H
+
+void dbf_dump_ASCII_BODY (DBF *dbf, ui32 RecNumber);
+void dbf_dump_ASCII_HEAD (DBF *dbf);
+void dbf_dump_ASCII_TAIL (DBF *dbf);
+
+#endif // _DUMP_ASCII_H
+
